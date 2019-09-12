28bdc927635e3891500217ff3695b40c77828690
d1egoprog <diegoprog@hotmail.com>
2019-09-12 14:34:39 -0500

    Updating jue. 12/09/2019 on 14:34:39,30

--- extraction-quijote/GetQuijote.java
+++ extraction-quijote/GetQuijote.java
@@ -2,33 +2,31 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.URL;
-import java.util.Vector;
 
 public class GetQuijote {
 	public static void main(String[] args) {
-		byte pag = 1;
+		int pag = 0;
 		while (pag <= 35) {
 			try {
-				String pagina = "http://www.elmundo.es/quijote/capitulo.html?cual=" + pag;
-				PrintWriter writer = new PrintWriter("Capitulo"+pag+".txt", "UTF-8");
+				String pagina = "https://www.elmundo.es/quijote/capitulo.html?cual=" + pag;
 				URL url = new URL(pagina);
+				
 				InputStreamReader reader = new InputStreamReader(url.openStream());
 				BufferedReader in = new BufferedReader(reader);
 				String inputLine;
-				pag++;
-				Vector<String> v = new Vector<String>();
+				StringBuilder sb = new StringBuilder();
 				while ((inputLine = in.readLine()) != null) {
-					v.add(inputLine);
+					System.out.println(inputLine);
+					sb.append(inputLine);
 				}
-				
-				for (int i = 0; i < v.size(); i++) {
-					System.out.println(v.get(i));
-					writer.println(v.get(i));
-				}
-				
+				 
+				PrintWriter writer = new PrintWriter("Capitulo"+pag+".txt", "UTF-8");
+				writer.write(sb.toString());
+				writer.flush();
 				writer.close();
+				
 				in.close();
-
+				pag++;
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
